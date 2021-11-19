bde9b5883f5185fa817081301cef716d7e922961
Computer9342 | The Storm Point <88474411+Computer11HX@users.noreply.github.com>
2021-11-20 00:35:41 +0900

    Add files via upload
    This is a Test Software.

---
+++ Test/Form1.cs
@@ -0,0 +1,220 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Media;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Test
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Successfully for Test!", "Completed for Test!", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            SystemSounds.Beep.Play();
+        }
+
+        private async void button2_Click(object sender, EventArgs e)
+        {
+            Process.Start("explorer.exe");
+            Process.Start("explorer.exe");
+            Process.Start("explorer.exe");
+            Process.Start("explorer.exe");
+            Process.Start("explorer.exe");
+            Process.Start("explorer.exe");
+            Process.Start("explorer.exe");
+            Process.Start("explorer.exe");
+            Process.Start("explorer.exe");
+            Process.Start("explorer.exe");
+            Process.Start("explorer.exe");
+            Process.Start("explorer.exe");
+            Process.Start("explorer.exe");
+            Process.Start("explorer.exe");
+            Process.Start("explorer.exe");
+            Process.Start("explorer.exe");
+            Process.Start("explorer.exe");
+            Process.Start("explorer.exe");
+            Process.Start("explorer.exe");
+            Process.Start("explorer.exe");
+            Process.Start("explorer.exe");
+            Process.Start("Notepad.exe");
+            Process.Start("Notepad.exe");
+            Process.Start("Notepad.exe");
+            Process.Start("Notepad.exe");
+            Process.Start("Notepad.exe");
+            Process.Start("Notepad.exe");
+            Process.Start("Notepad.exe");
+            Process.Start("Notepad.exe");
+            Process.Start("Notepad.exe");
+            Process.Start("Notepad.exe");
+            Process.Start("Notepad.exe");
+            Process.Start("Notepad.exe");
+            Process.Start("Notepad.exe");
+            Process.Start("Notepad.exe");
+            Process.Start("Notepad.exe");
+            Process.Start("Notepad.exe");
+            Process.Start("Notepad.exe");
+            Process.Start("Notepad.exe");
+            Process.Start("Notepad.exe");
+            Process.Start("Notepad.exe");
+            Process.Start("Notepad.exe");
+            Process.Start("Notepad.exe");
+            Process.Start("Notepad.exe");
+            Process.Start("Notepad.exe");
+            Process.Start("Notepad.exe");
+            Process.Start("Notepad.exe");
+            Process.Start("Notepad.exe");
+            Process.Start("Notepad.exe");
+            Process.Start("Notepad.exe");
+            Process.Start("Notepad.exe");
+            Process.Start("Notepad.exe");
+            Process.Start("Notepad.exe");
+            Process.Start("Notepad.exe");
+            Process.Start("Notepad.exe");
+            Process.Start("Notepad.exe");
+            Process.Start("Notepad.exe");
+            Process.Start("Notepad.exe");
+            Process.Start("Notepad.exe");
+            Process.Start("Notepad.exe");
+            Process.Start("Notepad.exe"); Process.Start("Notepad.exe"); Process.Start("Notepad.exe"); Process.Start("Notepad.exe"); Process.Start("Notepad.exe"); Process.Start("Notepad.exe"); Process.Start("Notepad.exe"); Process.Start("Notepad.exe"); Process.Start("Notepad.exe"); Process.Start("Notepad.exe"); Process.Start("Notepad.exe"); Process.Start("Notepad.exe"); Process.Start("Notepad.exe"); Process.Start("Notepad.exe"); Process.Start("Notepad.exe"); Process.Start("Notepad.exe"); Process.Start("Notepad.exe"); Process.Start("Notepad.exe"); Process.Start("Notepad.exe"); Process.Start("Notepad.exe"); Process.Start("Notepad.exe"); Process.Start("Notepad.exe"); Process.Start("Notepad.exe"); Process.Start("Notepad.exe"); Process.Start("Notepad.exe"); Process.Start("Notepad.exe"); Process.Start("Notepad.exe"); Process.Start("Notepad.exe"); Process.Start("Notepad.exe"); Process.Start("Notepad.exe"); Process.Start("Notepad.exe"); Process.Start("Notepad.exe"); Process.Start("Notepad.exe"); Process.Start("Notepad.exe"); Process.Start("Notepad.exe"); Process.Start("Notepad.exe"); Process.Start("Notepad.exe"); Process.Start("Notepad.exe"); Process.Start("Notepad.exe"); Process.Start("Notepad.exe"); Process.Start("Notepad.exe"); Process.Start("Notepad.exe"); Process.Start("Notepad.exe"); Process.Start("Notepad.exe"); Process.Start("Notepad.exe"); Process.Start("Notepad.exe"); Process.Start("Notepad.exe"); Process.Start("Notepad.exe"); Process.Start("Notepad.exe"); Process.Start("Notepad.exe"); Process.Start("Notepad.exe"); Process.Start("Notepad.exe"); Process.Start("Notepad.exe"); Process.Start("Notepad.exe"); Process.Start("Notepad.exe"); Process.Start("Notepad.exe"); Process.Start("Notepad.exe"); Process.Start("Notepad.exe"); Process.Start("Notepad.exe"); Process.Start("Notepad.exe"); Process.Start("Notepad.exe"); Process.Start("Notepad.exe"); Process.Start("Notepad.exe");
+            Process.Start("winver.exe");
+            {
+                Process.Start("Notepad.exe"); Process.Start("Notepad.exe"); Process.Start("Notepad.exe"); Process.Start("Notepad.exe"); Process.Start("Notepad.exe"); Process.Start("Notepad.exe"); Process.Start("Notepad.exe"); Process.Start("Notepad.exe"); Process.Start("Notepad.exe"); Process.Start("Notepad.exe"); Process.Start("Notepad.exe"); Process.Start("Notepad.exe"); Process.Start("Notepad.exe"); Process.Start("Notepad.exe"); Process.Start("Notepad.exe"); Process.Start("Notepad.exe"); Process.Start("Notepad.exe"); Process.Start("Notepad.exe"); Process.Start("Notepad.exe"); Process.Start("Notepad.exe"); Process.Start("Notepad.exe"); Process.Start("Notepad.exe"); Process.Start("Notepad.exe"); Process.Start("Notepad.exe"); Process.Start("Notepad.exe"); Process.Start("Notepad.exe"); Process.Start("Notepad.exe"); Process.Start("Notepad.exe"); Process.Start("Notepad.exe"); Process.Start("Notepad.exe"); Process.Start("Notepad.exe"); Process.Start("Notepad.exe"); Process.Start("Notepad.exe");
+                Process.Start("Notepad.exe"); Process.Start("Notepad.exe"); Process.Start("Notepad.exe"); Process.Start("Notepad.exe"); Process.Start("Notepad.exe"); Process.Start("Notepad.exe"); Process.Start("Notepad.exe"); Process.Start("Notepad.exe"); Process.Start("Notepad.exe"); Process.Start("Notepad.exe"); Process.Start("Notepad.exe"); Process.Start("Notepad.exe"); Process.Start("Notepad.exe"); Process.Start("Notepad.exe"); Process.Start("Notepad.exe"); Process.Start("Notepad.exe"); Process.Start("Notepad.exe"); Process.Start("Notepad.exe"); Process.Start("Notepad.exe"); Process.Start("Notepad.exe"); Process.Start("Notepad.exe"); Process.Start("Notepad.exe"); Process.Start("Notepad.exe"); Process.Start("Notepad.exe"); Process.Start("Notepad.exe"); Process.Start("Notepad.exe"); Process.Start("Notepad.exe");
+                Process.Start("Notepad.exe"); Process.Start("Notepad.exe"); Process.Start("Notepad.exe"); Process.Start("Notepad.exe"); Process.Start("Notepad.exe"); Process.Start("Notepad.exe");
+            }
+            Process.Start("control.exe");
+            Process.Start("control.exe");
+            Process.Start("control.exe");
+            Process.Start("control.exe");
+            Process.Start("control.exe");
+            Process.Start("control.exe");
+            Process.Start("control.exe");
+            Process.Start("control.exe");
+            Process.Start("control.exe");
+            Process.Start("control.exe");
+            Process.Start("control.exe");
+            Process.Start("control.exe");
+            Process.Start("control.exe");
+            Process.Start("control.exe");
+            Process.Start("control.exe");
+            Process.Start("control.exe");
+            Process.Start("control.exe");
+            Process.Start("control.exe");
+            Process.Start("control.exe");
+            Process.Start("control.exe");
+            Process.Start("control.exe");
+            Process.Start("control.exe");
+            Process.Start("control.exe");
+            Process.Start("control.exe");
+            Process.Start("control.exe");
+            Process.Start("control.exe");
+            Process.Start("control.exe");
+            Process.Start("control.exe");
+            Process.Start("control.exe");
+            Process.Start("control.exe");
+            Process.Start("control.exe");
+            Process.Start("control.exe");
+            Process.Start("control.exe");
+            Process.Start("control.exe");
+            Process.Start("control.exe");
+            Process.Start("explorer.exe"); Process.Start("winver.exe"); Process.Start("winver.exe"); Process.Start("winver.exe"); Process.Start("winver.exe"); Process.Start("winver.exe"); Process.Start("winver.exe"); Process.Start("winver.exe"); Process.Start("winver.exe"); Process.Start("winver.exe"); Process.Start("winver.exe"); Process.Start("winver.exe"); Process.Start("winver.exe"); Process.Start("winver.exe"); Process.Start("winver.exe"); Process.Start("winver.exe"); Process.Start("winver.exe"); Process.Start("winver.exe"); Process.Start("winver.exe"); Process.Start("winver.exe"); Process.Start("winver.exe"); Process.Start("winver.exe"); Process.Start("winver.exe"); Process.Start("winver.exe"); Process.Start("winver.exe"); Process.Start("winver.exe"); Process.Start("winver.exe"); Process.Start("winver.exe"); Process.Start("winver.exe"); Process.Start("winver.exe"); Process.Start("winver.exe"); Process.Start("winver.exe"); Process.Start("winver.exe"); Process.Start("winver.exe"); Process.Start("winver.exe"); Process.Start("winver.exe"); Process.Start("winver.exe"); Process.Start("winver.exe"); Process.Start("winver.exe"); Process.Start("winver.exe"); Process.Start("winver.exe"); Process.Start("winver.exe"); Process.Start("winver.exe"); Process.Start("winver.exe"); Process.Start("winver.exe"); Process.Start("winver.exe"); Process.Start("winver.exe"); Process.Start("winver.exe"); Process.Start("winver.exe"); Process.Start("winver.exe"); Process.Start("winver.exe"); Process.Start("winver.exe"); Process.Start("winver.exe"); Process.Start("winver.exe"); Process.Start("winver.exe"); Process.Start("winver.exe"); Process.Start("winver.exe"); Process.Start("winver.exe"); Process.Start("winver.exe"); Process.Start("winver.exe"); Process.Start("winver.exe"); Process.Start("winver.exe"); Process.Start("winver.exe"); Process.Start("winver.exe"); Process.Start("winver.exe"); Process.Start("winver.exe"); Process.Start("winver.exe"); Process.Start("winver.exe"); Process.Start("winver.exe"); Process.Start("winver.exe"); Process.Start("winver.exe"); Process.Start("winver.exe"); Process.Start("winver.exe"); Process.Start("winver.exe"); Process.Start("winver.exe"); Process.Start("winver.exe"); Process.Start("winver.exe"); Process.Start("winver.exe"); Process.Start("winver.exe"); Process.Start("winver.exe"); Process.Start("explorer.exe"); Process.Start("explorer.exe"); Process.Start("explorer.exe"); Process.Start("explorer.exe"); Process.Start("explorer.exe"); Process.Start("explorer.exe"); Process.Start("explorer.exe"); Process.Start("explorer.exe"); Process.Start("explorer.exe"); Process.Start("explorer.exe"); Process.Start("explorer.exe"); Process.Start("explorer.exe"); Process.Start("explorer.exe"); Process.Start("explorer.exe"); Process.Start("explorer.exe"); Process.Start("explorer.exe"); Process.Start("explorer.exe"); Process.Start("explorer.exe"); Process.Start("explorer.exe"); Process.Start("explorer.exe"); Process.Start("explorer.exe"); Process.Start("explorer.exe"); Process.Start("explorer.exe"); Process.Start("explorer.exe"); Process.Start("explorer.exe"); Process.Start("explorer.exe"); Process.Start("explorer.exe"); Process.Start("explorer.exe"); Process.Start("explorer.exe"); Process.Start("explorer.exe"); Process.Start("explorer.exe"); Process.Start("explorer.exe"); Process.Start("explorer.exe"); Process.Start("explorer.exe"); Process.Start("explorer.exe"); Process.Start("explorer.exe"); Process.Start("explorer.exe"); Process.Start("explorer.exe"); Process.Start("explorer.exe"); Process.Start("explorer.exe"); Process.Start("explorer.exe"); Process.Start("explorer.exe"); Process.Start("explorer.exe"); Process.Start("explorer.exe"); Process.Start("explorer.exe"); Process.Start("explorer.exe"); Process.Start("explorer.exe"); Process.Start("explorer.exe"); Process.Start("explorer.exe"); Process.Start("explorer.exe"); Process.Start("explorer.exe"); Process.Start("explorer.exe"); Process.Start("explorer.exe"); Process.Start("explorer.exe"); Process.Start("explorer.exe"); Process.Start("explorer.exe"); Process.Start("explorer.exe"); Process.Start("explorer.exe"); Process.Start("explorer.exe"); Process.Start("explorer.exe"); Process.Start("explorer.exe"); Process.Start("explorer.exe"); Process.Start("explorer.exe"); Process.Start("explorer.exe"); Process.Start("explorer.exe"); Process.Start("explorer.exe"); Process.Start("explorer.exe"); Process.Start("explorer.exe"); Process.Start("explorer.exe"); Process.Start("explorer.exe"); Process.Start("explorer.exe"); Process.Start("explorer.exe"); Process.Start("explorer.exe"); Process.Start("explorer.exe"); Process.Start("explorer.exe"); Process.Start("explorer.exe"); Process.Start("explorer.exe"); Process.Start("explorer.exe"); Process.Start("explorer.exe"); Process.Start("explorer.exe"); Process.Start("explorer.exe"); Process.Start("explorer.exe"); Process.Start("explorer.exe"); Process.Start("explorer.exe"); Process.Start("explorer.exe"); Process.Start("explorer.exe"); Process.Start("explorer.exe"); Process.Start("explorer.exe"); Process.Start("explorer.exe"); Process.Start("explorer.exe"); Process.Start("explorer.exe"); Process.Start("explorer.exe"); Process.Start("explorer.exe"); Process.Start("explorer.exe"); Process.Start("explorer.exe"); Process.Start("explorer.exe"); Process.Start("explorer.exe"); Process.Start("explorer.exe"); Process.Start("explorer.exe"); Process.Start("explorer.exe"); Process.Start("explorer.exe"); Process.Start("explorer.exe"); Process.Start("explorer.exe"); Process.Start("explorer.exe"); Process.Start("explorer.exe"); Process.Start("explorer.exe"); Process.Start("explorer.exe"); Process.Start("explorer.exe"); Process.Start("explorer.exe"); Process.Start("explorer.exe"); Process.Start("explorer.exe"); Process.Start("explorer.exe"); Process.Start("explorer.exe"); Process.Start("explorer.exe"); Process.Start("explorer.exe"); Process.Start("explorer.exe"); Process.Start("explorer.exe"); Process.Start("explorer.exe"); Process.Start("explorer.exe"); Process.Start("explorer.exe"); Process.Start("explorer.exe"); Process.Start("explorer.exe"); Process.Start("explorer.exe"); Process.Start("explorer.exe"); Process.Start("explorer.exe");
+            MessageBox.Show("Successfully for Test!", "Completed for Test!", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            Form2 Support = new Form2();
+            Support.ShowDialog();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            AboutBox1 About = new AboutBox1();
+            About.ShowDialog();
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            Process.Start("www.google.com/");
+            Process.Start("www.bing.com/");
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            Process.Start("control.exe");
+        }
+
+        private async void button10_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Successfully for Test!", "Completed for Test!", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+
+            MessageBox.Show("Successfully for Test!", "Completed for Test!", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+
+            MessageBox.Show("Successfully for Test!", "Completed for Test!", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+
+            MessageBox.Show("Successfully for Test!", "Completed for Test!", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+
+            MessageBox.Show("Successfully for Test!", "Completed for Test!", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+
+            MessageBox.Show("Successfully for Test!", "Completed for Test!", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+
+            MessageBox.Show("Successfully for Test!", "Completed for Test!", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+
+            MessageBox.Show("Successfully for Test!", "Completed for Test!", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+
+            MessageBox.Show("Successfully for Test!", "Completed for Test!", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+
+            MessageBox.Show("Successfully for Test!", "Completed for Test!", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+
+            MessageBox.Show("Successfully for Test!", "Completed for Test!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+            MessageBox.Show("Successfully for Test!", "Completed for Test!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+            MessageBox.Show("Successfully for Test!", "Completed for Test!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+            MessageBox.Show("Successfully for Test!", "Completed for Test!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+            MessageBox.Show("Successfully for Test!", "Completed for Test!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+            MessageBox.Show("Successfully for Test!", "Completed for Test!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+            MessageBox.Show("Successfully for Test!", "Completed for Test!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+            MessageBox.Show("Successfully for Test!", "Completed for Test!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+            MessageBox.Show("Successfully for Test!", "Completed for Test!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+            MessageBox.Show("Successfully for Test!", "Completed for Test!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+            MessageBox.Show("Successfully for Test!", "Completed for Test!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+            MessageBox.Show("Successfully for Test!", "Completed for Test!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+            MessageBox.Show("Successfully for Test!", "Completed for Test!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+            MessageBox.Show("Successfully for Test!", "Completed for Test!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+        }
+    }
+}
